8a5a24d9df3b07d0e85fc30f4f971219a47603c1
zalmi <78726819+zalmicoding@users.noreply.github.com>
2022-10-04 09:18:23 -0700

    pos comleted

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -1,26 +1,37 @@
 <div class="deznav">
     <div class="deznav-scroll">
         <ul class="metismenu" id="menu">
+            <li><a  href="{{route('products.index')}}" aria-expanded="false">
+                <i class="fas fa-warehouse"></i>
+                 <span class="nav-text">Products</span>
+             </a>
 
+         </li>
             <li><a  href="{{url('transcation')}}" aria-expanded="false">
                     <i class="flaticon-381-television"></i>
-                    <span class="nav-text">pos</span>
+                    <span class="nav-text">Pos</span>
                 </a>
 
             </li>
 
-            <li><a  href="{{route('products.index')}}" aria-expanded="false">
-                   <i class="fas fa-warehouse"></i>
-                    <span class="nav-text">products</span>
-                </a>
 
-            </li>
             <li><a  href="{{url('/transcation/history')}}" aria-expanded="false">
                <i class="far fa-file"></i>
-                <span class="nav-text">sales reports</span>
+                <span class="nav-text">Sales reports</span>
             </a>
 
            </li>
+        <li>  <a class="dropdown-item" href="{{ route('logout') }}"
+            onclick="event.preventDefault();
+            document.getElementById('logout-form').submit();">
+
+            <i class="far fa-logout"></i>
+            <span class="nav-text">logout</span>
+            </a>
+            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+            @csrf
+            </form>
+        </li>
 
 
 
